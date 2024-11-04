cddc8758e5dd0e302421891b535af291b718d9ca
alenavarroxp <alejandropvp13@gmail.com>
2024-11-04 23:45:33 +0100

    deno deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
+import deno from '@deno/astro-adapter';
 import robotsTxt from "astro-robots-txt"
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: 'https://alenavarro.dev/',
+  output: 'server',
+  adapter: deno(),
 })
