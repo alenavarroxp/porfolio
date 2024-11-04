c54ae522c10a7fe83a2f4be19aed0ff94520ecd7
alenavarroxp <alejandropvp13@gmail.com>
2024-11-05 00:06:25 +0100

    deno deploy2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,6 @@ import robotsTxt from "astro-robots-txt"
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://alenavarro.dev/',
   output: 'server',
   adapter: deno(),
 })
